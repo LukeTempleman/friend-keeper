3586e62b425f0041971274a819e63c89ff229d4b
Luke Templeman <luke1.temp20@gmail.com>
2022-09-10 09:32:51 +0200

    ✅ Small test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+//Basic React Testing
+test('Heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const element = screen.getByText(/Friend-Keeper/i)
+  expect(element).toBeInTheDocument()
+
+})
